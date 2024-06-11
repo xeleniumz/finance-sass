ef40f43c870279dff4eca8e0ed5ce8b62313a861
xeleniumz <demolismz@windowslive.com>
2024-06-11 16:39:02 +0700

    update return data from get account api

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,5 +1,4 @@
 import { eq } from "drizzle-orm";
-import { HTTPException } from "hono/http-exception";
 import { Hono } from "hono";
 import { db } from "@/db/drizzle";
 import { accounts } from "@/db/schema";
@@ -9,10 +8,7 @@ const app = new Hono()
   .get("/", clerkMiddleware(), async (c) => {
     const auth = getAuth(c);
     if (!auth?.userId) {
-      // return c.json({ error: "Unauthorized" }, 401);
-      throw new HTTPException(401, {
-        res: c.json({ error: "Unauthorized" }, 401),
-      });
+      return c.json({ error: "Unauthorized" }, 401);
     }
     const data = await db
       .select({
